503c6bb1ff81ec855a85fd2991b0e1a35fadc208
FlorR566 <florodriguez566@gmail.com>
2025-12-04 21:25:19 -0300

    agregar estilos, eliminar archivos de componentes de root, agregar mensaje de desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import BannerPresentation from "./BannerPresentation";
+import CardContainer from "./CardContainer";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<main>
+				<Navbar />
+				<div className="aviso">
+					<h1>Página en desarrollo</h1>
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
