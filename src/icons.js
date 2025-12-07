80fa654f04571aad007f58e1c97132fdaf53011e
FlorR566 <florodriguez566@gmail.com>
2025-12-07 16:52:23 -0300

    Separar el array de íconos para redes sociales del footer

---
+++ src/icons.js
@@ -0,0 +1,39 @@
+import {
+	FaFacebookF,
+	FaInstagram,
+	FaLinkedinIn,
+	FaWhatsapp,
+} from "react-icons/fa";
+
+const socialIcons = [
+	{
+		id: 1,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		href: "https://facebook.com",
+		Icon: FaFacebookF,
+	},
+	{
+		id: 2,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		href: "https://wa.me/123456789",
+		Icon: FaWhatsapp,
+	},
+	{
+		id: 3,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		href: "https://instagram.com",
+		Icon: FaInstagram,
+	},
+	{
+		id: 4,
+		target: "_blank",
+		rel: "noopener noreferrer",
+		href: "https://linkedin.com",
+		Icon: FaLinkedinIn,
+	},
+];
+
+export default socialIcons;
