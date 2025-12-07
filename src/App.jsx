afd5f88e8eff67aa19d16df8cb066d0d2bd89133
FlorR566 <florodriguez566@gmail.com>
2025-12-07 16:48:59 -0300

    Agregar aviso de desarrollo en proceso

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -7,6 +8,8 @@ function App() {
 			<main>
 				<div className="aviso">
 					<h1>Página en desarrollo</h1>
+					<h3> 1º etapa: mobile</h3>
+					<h3> 2º etapa: desktop</h3>
 				</div>
 			</main>
 			<Footer />
