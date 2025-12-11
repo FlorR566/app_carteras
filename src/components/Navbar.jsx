baa77352869166bbc91179381cc0dbae29989dc6
FlorR566 <florodriguez566@gmail.com>
2025-12-11 13:00:45 -0300

    agregar resaltado en navlinks, agregar imagen del logo y afecto para cerrar en toggle menú

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import styles from "./Navbar.module.css";
+import logo from "../assets/Logo.png";
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
 
+	let statusMenu = open ? "X" : "☰";
+
 	return (
 		<nav className={styles.navbar}>
-			<div className={`${styles.navLogo} ${styles.ralewayTextLogo}`}>
-				Lady Valentina
+			<div className={styles.containerLogo}>
+				<img src={logo} className={styles.imgLogo} alt="Logo Lady Valentina" />
+				<h1 className={styles.ralewayTextLogo}>LADY VALENTINA</h1>
 			</div>
 
-			<div className={styles.navToggle} onClick={() => setOpen(!open)}>
-				☰
-			</div>
+			{/* <div className={styles.navToggle} onClick={() => setOpen(!open)}>
+				{statusMenu}
+			</div> */}
 
-			<ul className={`${styles.navLinks} ${open ? styles.open : ""}`}>
+			<button
+				className={styles.navToggle}
+				onClick={() => setOpen(!open)}
+				aria-label="Abrir o cerrar el menú"
+			>
+				{statusMenu}
+			</button>
+
+			<ul className={`${styles.navLinks} ${open && styles.open}`}>
 				<li>
 					<a href="#home">Inicio</a>
 				</li>
