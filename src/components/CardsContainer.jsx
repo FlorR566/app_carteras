a92e381d1e8ffd46fc7ce8366d59aafeb3434296
FlorR566 <florodriguez566@gmail.com>
2026-02-13 22:38:29 -0300

    feat: implement product details modal functionality
    - Add selectedProduct state and click events to Card component.
    - Integrate useLockBodyScroll hook to manage overflow.
    - Add responsive modal styles, backdrop-blur, and close button.
    - Map product images and details within the modal view.

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,29 +1,74 @@
+import { useState } from "react";
 import styles from "../components/CardsContainer.module.css";
 import useIsMobile from "../useIsMobile";
+import useLockBodyScroll from "../useLockBodyScroll";
 
 const Card = ({ bags, category }) => {
 	const isMobile = useIsMobile();
+	const [selectedProduct, setSelectedProduct] = useState(null);
+
+	useLockBodyScroll(!!selectedProduct);
+
 	return (
-		<div className={styles.cardsContainer}>
-			{bags.map((item) =>
-				category === item.category ? (
-					<div className={styles.card} key={item.id}>
-						<img src={item.images[0]} alt={item.name} />
+		<>
+			<div className={styles.cardsContainer}>
+				{bags.map((item) =>
+					category === item.category ? (
+						<div className={styles.card} key={item.id}>
+							<img
+								onClick={() => setSelectedProduct(item)}
+								src={item.images[0]}
+								alt={item.name}
+							/>
 
-						<h3>{item.name}</h3>
-						<p>$ {item.price}</p>
+							<h3>{item.name}</h3>
+							<p>$ {item.price}</p>
 
-						{isMobile ? (
-							<button className={styles.cardButton}>COMPRAR</button>
-						) : (
-							<button className={styles.cardButton}>VER PRODUCTO</button>
-						)}
+							{isMobile ? (
+								<button
+									className={styles.cardButton}
+									onClick={() => setSelectedProduct(item)}
+								>
+									COMPRAR
+								</button>
+							) : (
+								<button
+									className={styles.cardButton}
+									onClick={() => setSelectedProduct(item)}
+								>
+									VER PRODUCTO
+								</button>
+							)}
+						</div>
+					) : (
+						" "
+					),
+				)}
+			</div>
+
+			{selectedProduct && (
+				<div className={styles.modal} onClick={() => setSelectedProduct(null)}>
+					<div
+						className={styles.modalContent}
+						onClick={(e) => e.stopPropagation()}
+					>
+						<span
+							className={styles.closeButton}
+							onClick={() => setSelectedProduct(null)}
+						>
+							&times;
+						</span>
+						{selectedProduct.images.map((img) => (
+							<img src={img} />
+						))}
+						<h2>{selectedProduct.name}</h2>
+						<h3>Precio: $ {selectedProduct.price}</h3>
+						<p>{selectedProduct.characteristics}</p>
+						<h4 className={styles.measures}>{selectedProduct.measures}</h4>
 					</div>
-				) : (
-					" "
-				),
+				</div>
 			)}
-		</div>
+		</>
 	);
 };
 
